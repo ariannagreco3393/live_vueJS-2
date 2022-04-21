8e14a08666d78b1e8d1688f9e02a67052b55f925
ariannagreco3393 <a.greco3393@gmail.com>
2022-04-21 12:45:10 +0200

    venom commit

---
+++ live-1/app.js
@@ -0,0 +1,10 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        nomi: ['arianna', 'gianluca', 'alessandro']
+    }
+})
+
+/* 
+Stampiamo tutti gli elementi di un array tramite la direttiva v-for.
+*/
---
+++ live-2/app.js
@@ -0,0 +1,10 @@
+/* 
+Stampiamo i numeri da 1 a 100 tramite la direttiva v-for
+*/
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        
+    }
+})
